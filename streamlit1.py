fe0ffa7be5fd1f035a138b653b9f4b6f2e1f2839
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-04 01:24:13 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -170,6 +170,7 @@ if page == "Data Analytics / Engagement & Monetization Strategies":
     The analysis provides actionable insights into how player engagement and spending behaviors vary across different segments. By focusing on the high-value segments identified, targeted strategies can be developed to enhance player retention and increase revenue. Additionally, understanding the factors driving player churn during event periods can inform more effective engagement strategies for future events.
     """)
 
+st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/111', caption='Day-by-Day Churn Rate: Event 1 vs Event 2', width=1000)
 
 
 elif page == "Dashboard / Executive Business Insights":
