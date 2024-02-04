e97feb5b8a8de45ee3c742ca61a997961b125d53
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-03 21:33:12 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -3,49 +3,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Set the page configuration and custom CSS
 st.set_page_config(layout="wide", page_title="Jason Chang's Portfolio")
 st.markdown("""
-    <link href='https://fonts.googleapis.com/css?family=Bebas+Neue|Lato&display=swap' rel='stylesheet'>
-    <style>
-    .big-font {
-        font-family: 'Bebas Neue', cursive;
-        font-size:70px !important;
-        font-weight: 400;
-        color: Black;
-    }
-    .medium-font {
-        font-family: 'Bebas Neue', cursive;
-        font-size:40px !important;
-        font-weight: bold;
-        color: #D09E55;
-    }
-    .small-font {
-        font-family: 'Lato', sans-serif;
-        font-size:30px !important;
-        color: Black;
-    }
-    .reportview-container .main {
-        background-color: #1D262F;
-    }
-    .sidebar .sidebar-content {
-        background-color: #1D262F;
-        color: white;
-    }
-    .fa {
-        padding-right: 5px;
-    }
-    /* Adjust top padding of the main container for all pages */
-    .reportview-container .main .block-container {
-        padding-top: 1rem; /* Reduced padding to move content up */
-    }
-    </style>
+<link href='https://fonts.googleapis.com/css?family=Bebas+Neue|Lato&display=swap' rel='stylesheet'>
+<style>
+.big-font { font-family: 'Bebas Neue', cursive; font-size:70px !important; font-weight: 400; color: Black; }
+.medium-font { font-family: 'Bebas Neue', cursive; font-size:40px !important; font-weight: bold; color: #D09E55; }
+.small-font { font-family: 'Lato', sans-serif; font-size:30px !important; color: Black; }
+.reportview-container .main { background-color: #1D262F; }
+.sidebar .sidebar-content { background-color: #1D262F; color: white; }
+.fa { padding-right: 5px; }
+/* Adjust top padding of the main container for all pages */
+.reportview-container .main .block-container { padding-top: 1rem; }
+</style>
 """, unsafe_allow_html=True)
 
-# Header and other content setup
 st.markdown('<p class="big-font">JASON CHANG</p>', unsafe_allow_html=True)
 st.markdown('<p class="medium-font">PORTFOLIO</p>', unsafe_allow_html=True)
 st.markdown('<p class="small-font">Full Stack Senior Data Analyst</p>', unsafe_allow_html=True)
+
+
+
 
 with st.sidebar:
     st.markdown('<p class="medium-font">Navigation</p>', unsafe_allow_html=True)
@@ -59,25 +37,24 @@ def load_data(url):
     return data
 
 def assign_correct_bucket(games_played):
-    if games_played >= 1 and games_played <= 3:
-        return 'Very Low'
-    elif games_played >= 4 and games_played <= 5:
-        return 'Low'
-    elif games_played >= 6 and games_played <= 9:
-        return 'Medium'
-    elif games_played >= 10 and games_played <= 68:
-        return 'High'
-    else:
-        return 'Unknown'
+    if games_played >= 1 and games_played <= 3: return 'Very Low'
+    elif games_played >= 4 and games_played <= 5: return 'Low'
+    elif games_played >= 6 and games_played <= 9: return 'Medium'
+    elif games_played >= 10 and games_played <= 68: return 'High'
+    else: return 'Unknown'
 
 data_url = "https://raw.githubusercontent.com/jasonchang0102/Streamlit0102/main/RAWBliz.csv"
 data = load_data(data_url)
 
+
+
+
 if page == "Welcome":
-    st.markdown("### Welcome to my Portfolio")
-    st.write("""
-        As a Senior Data Analyst with a strong focus on integrating business strategy and transforming complex data into strategic assets, I have evolved from intricate statistical analysis to advanced predictive modeling. My expertise lies in turning vast datasets into actionable insights. Committed to pioneering data-driven research, I aim to lead innovative strategies in a dynamic corporate setting. My goal is to drive organizational success and innovation by leveraging data intelligence for business growth and collaborative leadership.
+    st.markdown("### Welcome to My Portfolio")
+    st.markdown("""
+    As a Senior Data Analyst with a strong focus on integrating business strategy and transforming complex data into strategic assets, I have evolved from intricate statistical analysis to advanced predictive modeling. My expertise lies in turning vast datasets into actionable insights. Committed to pioneering data-driven research, I aim to lead innovative strategies in a dynamic corporate setting. My goal is to drive organizational success and innovation by leveraging data intelligence for business growth and collaborative leadership.
     """)
+
 elif page == "Data Analytics / Engagement & Monetization Strategies":
     st.header("Data Analytics / Engagement & Monetization Strategies")
     st.subheader("Executive Summary/Business Objective:")
@@ -85,18 +62,30 @@ elif page == "Data Analytics / Engagement & Monetization Strategies":
     Emphasized maximizing revenue and enhancing player engagement and satisfaction by analyzing behavior and spending patterns during Warcraft's two in-game events.
     """)
 
-    # Include the images from GitHub with specified width
+    st.subheader("Findings/Strategic Implications:")
+    st.write("""
+    Identified high-spending segments, especially in Platform 3, Region 1, signaling a priority for future promotions. Observed low spending in Platform 1, Region 5, highlighting the necessity for further research and adjustments to the strategy.
+    """)
+
+    st.subheader("Research Question/Data Exploration:")
+    st.write("""
+    Conducted exploratory data analysis to understand player spending behavior, emphasizing games played, skill levels, dollars spent, and items crafted.
+    """)
+
+    # Images from GitHub
     common_width = 1000
-    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/111.png', caption='Average Spending by Number of Games Played: Event 1 vs Event 2', width=common_width)
-    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/222.png', caption='Day-by-Day Churn Rate: Event 1 vs Event 2', width=common_width)
     st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/333.png', caption='Distribution of Spending Across Skill Brackets', width=common_width)
+    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/222.png', caption='Day-by-Day Churn Rate: Event 1 vs Event 2', width=common_width)
+    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/111.png', caption='Average Spending by Number of Games Played: Event 1 vs Event 2', width=common_width)
+
+    # Continue with Methodology/Analytical Proficiency and further analysis
+
 
     st.subheader("Methodology/Analytical Proficiency:")
     st.write("""
     Leveraged Python, K-Means Clustering, and heatmap analysis for an in-depth comparative study of player engagement and spending. Implemented segmentation based on in-game behavior for a comprehensive analysis.
     """)
 
-    # Visualizations for Data Analytics
     event_1_start, event_1_end = pd.Timestamp('2017-01-24'), pd.Timestamp('2017-02-14')
     event_2_start, event_2_end = pd.Timestamp('2017-02-28'), pd.Timestamp('2017-03-21')
 
@@ -104,7 +93,7 @@ elif page == "Data Analytics / Engagement & Monetization Strategies":
     event_2_data = data[(data['Date'] >= event_2_start) & (data['Date'] <= event_2_end)]
 
     sns.set_style("whitegrid")
-    fig, axes = plt.subplots(2, 2, figsize=(13, 10))  # Adjusted for a common width
+    fig, axes = plt.subplots(2, 2, figsize=(13, 10)) # Adjusted for a common width of 1300 pixels
 
     sns.kdeplot(event_1_data['games_played'], shade=True, color="skyblue", label="Event 1", ax=axes[0, 0])
     sns.kdeplot(event_2_data['games_played'], shade=True, color="salmon", label="Event 2", ax=axes[0, 0])
@@ -125,13 +114,18 @@ elif page == "Data Analytics / Engagement & Monetization Strategies":
     plt.tight_layout()
     st.pyplot(fig)
 
+
+
     heatmap_data = data.groupby(['region', 'platform']).dollars_spent.mean().unstack()
-    plt.figure(figsize=(13, 8))
+    plt.figure(figsize=(13, 8)) # Adjusted for a common width of 1300 pixels
     sns.heatmap(heatmap_data, annot=True, cmap="YlGnBu", fmt=".2f", linewidths=.5)
     plt.title("Average Dollars Spent per Player by Region and Platform")
     st.pyplot(plt)
 
-
+    st.subheader("Conclusion and Strategic Insights:")
+    st.write("""
+    The analysis provides actionable insights into how player engagement and spending behaviors vary across different segments. By focusing on the high-value segments identified, targeted strategies can be developed to enhance player retention and increase revenue. Additionally, understanding the factors driving player churn during event periods can inform more effective engagement strategies for future events.
+    """)
 
 
 
@@ -192,6 +186,8 @@ elif page == "Contact":
     - **Email:** [jason.chang01022021@gmail.com](mailto:jason.chang01022021@gmail.com)
     - **LinkedIn:** [linkedin.com/in/jchang0102](https://linkedin.com/in/jchang0102)
     """, unsafe_allow_html=True)
+
+
 
 
 
