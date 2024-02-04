6408f4e0e0188c67f1ba998e7de54a866f99f859
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-04 01:42:09 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -21,6 +21,7 @@ st.markdown("""
 <link href='https://fonts.googleapis.com/css?family=Bebas+Neue|Lato&display=swap' rel='stylesheet'>
 <style>
 .big-font { font-family: 'Bebas Neue', cursive; font-size:100px !important; font-weight: 400; color: Black; }
+.biger-font { font-family: 'Bebas Neue', cursive; font-size:80px !important; font-weight: 400; color: Black; }
 .medium-font { font-family: 'Bebas Neue', cursive; font-size:60px !important; font-weight: bold; color: #D09E55; }
 .small-font { font-family: 'Bahnschrift ', sans-serif; font-size:30px !important; color: Black; }
 .reportview-container .main { background-color: #1D262F; }
@@ -32,7 +33,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown('<p class="big-font">JASON CHANG</p>', unsafe_allow_html=True)
-st.markdown('<p class="medium-font">PORTFOLIO</p>', unsafe_allow_html=True)
+st.markdown('<p class="biger-font">PORTFOLIO</p>', unsafe_allow_html=True)
 st.markdown('<p class="medium-font">Full Stack Senior Data Analyst</p>', unsafe_allow_html=True)
 
 
