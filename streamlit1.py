3a9bd54fb9bf63c040046fafb82637edbbfcbd9b
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-03 20:01:59 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -11,7 +11,7 @@ st.markdown("""
 .big-font { font-size:50px !important; font-weight: bold; color: white; }
 .medium-font { font-size:35px !important; color: #a47321; }
 .small-font { font-size:25px !important; color: white; }
-.reportview-container .main { background-color: #1d262f; }
+.reportview-container .main { background-color: #1D262F; }
 .sidebar .sidebar-content { background-color: #1d262f; color: white; }
 .fa { padding-right: 5px; }
 </style>
