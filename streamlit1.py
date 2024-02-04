0a179bfef168274ec03586d59ff65f78b1e7c6ea
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-03 19:59:19 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -184,3 +184,8 @@ elif page == "Contact":
     - **Email:** [jason.chang01022021@gmail.com](mailto:jason.chang01022021@gmail.com)
     - **LinkedIn:** [linkedin.com/in/jchang0102](https://linkedin.com/in/jchang0102)
     """, unsafe_allow_html=True)
+
+
+
+
+
