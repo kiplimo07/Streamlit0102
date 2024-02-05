9eb9a077023d67abbf673176c6bb58ba2b4e661c
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-04 19:37:59 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -229,14 +229,16 @@ elif page == "Process Automation / Quarterly Royalty Management":
     st.markdown("""
     **Objective:** Streamline the quarterly royalty management process, reducing time spent by financial analysts from a month-long task to a 2-hour automated process.
     """)
-    # Place the st.image call correctly outside the markdown string
     st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/2222', width=800)
+    # Add another image under the first one
+    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/9999', width=800)
 
     st.markdown("""
     **Impact:** Achieved an 85% reduction in process time, significantly lowering operational costs and enhancing efficiency and accuracy in royalty management.
     
     **Methodology:** Developed a custom Python script for data consolidation and utilized Excel VBA for automating data extraction and report generation.
     """)
+
 
 
 elif page == "Scope of Skills":
