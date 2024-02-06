98a9bdab68dc528c721fe1eabdd654762d7398bc
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-05 16:22:54 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -22,7 +22,7 @@ st.markdown("""
 <style>
 .big-font {
     font-family: 'Bebas Neue', sans-serif;
-     font-size: 70px !important; /* Reduced size for shorter appearance */
+     font-size: 100px !important; /* Reduced size for shorter appearance */
     font-weight: 100;
     color: white;
     display: inline-block; /* Makes the element respect the width */
