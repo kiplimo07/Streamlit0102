4b6cc571143e8bb2c6f6d5811196c5674d32324d
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-05 16:27:44 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -32,7 +32,7 @@ st.markdown("""
 .big2-font { font-family: 'Bebas Neue', sans-serif; font-size:70px !important; font-weight: 100; color: white; display: inline-block; margin-bottom: 0px; }
 .med2-font { font-family: 'Bebas Neue', sans-serif; font-size:45px !important; font-weight: 100; color: #D09E55; }
 .medium-font { font-family: 'Bebas Neue', sans-serif; font-size:55px !important; font-weight: 100; color: #D09E55; }
-.small-font { font-family: 'Lato ', sans-serif; font-size:35px !important; color: white; }
+.small-font { font-family: 'Lato ', sans-serif; font-size:45px !important; color: white; }
 .reportview-container .main { background-color: #1D262F; }
 .sidebar .sidebar-content { background-color: #1D262F; color: white; }
 .fa { padding-right: 15px; }
