160106a267595668ec7b3d15c838806e64e94a14
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-06 10:01:37 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -31,8 +31,8 @@ st.markdown("""
 }
 .big2-font { font-family: 'Bebas Neue', sans-serif; font-size:70px !important; font-weight: 100; color: #282D33 ; display: inline-block; margin-bottom: 0px; }
 .med2-font { font-family: 'Bebas Neue', sans-serif; font-size:45px !important; font-weight: 100; color: #D09E55; }
-.medium-font { font-family: 'Bebas Neue', sans-serif; font-size:55px !important; font-weight: 100; color: #D09E55; }
-.small-font { font-family: 'Lato', sans-serif; font-size:45px !important; color: #282D33; }
+.medium-font { font-family: 'Bebas Neue', sans-serif; font-size:40px !important; font-weight: 100; color: #D09E55; }
+.small-font { font-family: 'Lato', sans-serif; font-size 35px !important; color: #282D33; }
 /* Apply Lato font and adjust size for markdown elements */
 .markdown, .streamlit-container .markdown-text-container * {
     font-family: 'Lato', sans-serif !important;
