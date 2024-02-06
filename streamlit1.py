8c45fbbf4693cc97141826627dbba2eca19a8e96
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-05 19:48:55 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -20,6 +20,14 @@ st.set_page_config(layout="wide", page_title="Jason Chang's Portfolio")
 st.markdown("""
 <link href='https://fonts.googleapis.com/css?family=Bebas+Neue|Lato&display=swap' rel='stylesheet'>
 <style>
+html, body, [data-testid="stApp"] {
+    background-color: #1D262F !important;
+}
+
+.stSidebar > div:first-child, .stSidebar > div:first-child > div:first-child {
+    background-color: #1D262F !important;
+}
+
 .big-font {
     font-family: 'Bebas Neue', sans-serif;
     font-size: 110px !important;
@@ -29,18 +37,47 @@ st.markdown("""
     max-width: 70%;
     margin: 0 auto;
 }
-.big2-font { font-family: 'Bebas Neue', sans-serif; font-size:70px !important; font-weight: 100; color: white; display: inline-block; margin-bottom: 0px; }
-.med2-font { font-family: 'Bebas Neue', sans-serif; font-size:45px !important; font-weight: 100; color: #D09E55; }
-.medium-font { font-family: 'Bebas Neue', sans-serif; font-size:55px !important; font-weight: 100; color: #D09E55; }
-.small-font { font-family: 'Lato', sans-serif; font-size:45px !important; color: white; }
+
+.big2-font { 
+    font-family: 'Bebas Neue', sans-serif; 
+    font-size:70px !important; 
+    font-weight: 100; 
+    color: white; 
+    display: inline-block; 
+    margin-bottom: 0px; 
+}
+
+.med2-font { 
+    font-family: 'Bebas Neue', sans-serif; 
+    font-size:45px !important; 
+    font-weight: 100; 
+    color: #D09E55; 
+}
+
+.medium-font { 
+    font-family: 'Bebas Neue', sans-serif; 
+    font-size:55px !important; 
+    font-weight: 100; 
+    color: #D09E55; 
+}
+
+.small-font { 
+    font-family: 'Lato', sans-serif; 
+    font-size:45px !important; 
+    color: white; 
+}
+
 /* Apply Lato font and adjust size for markdown elements */
 .markdown, .streamlit-container .markdown-text-container * {
     font-family: 'Lato', sans-serif !important;
     font-size: 20px !important;
+    color: white !important;
 }
-.reportview-container .main { background-color: #1D262F; }
-.sidebar .sidebar-content { background-color: #1D262F; color: white; }
-.fa { padding-right: 15px; }
+
+.fa { 
+    padding-right: 15px; 
+}
+
 hr { 
     border-top: 1px solid #FFFFFF;
     width: 80%;
@@ -52,12 +89,12 @@ hr {
 """, unsafe_allow_html=True)
 
 st.markdown('<p class="big-font">JASON CHANG</p>', unsafe_allow_html=True)
-st.markdown('<div><p class="big2-font">PORTFOLIO</p><hr></div>', unsafe_allow_html=True)  # Adjusted for closer spacing and alignment
+st.markdown('<div><p class="big2-font">PORTFOLIO</p><hr></div>', unsafe_allow_html=True)
 st.markdown('<p class="med2-font">Full Stack Senior Data Analyst</p>', unsafe_allow_html=True)
 
 st.markdown("""
 <style>
-    /* Other styles */
+    /* Adjusting the main content area */
     .reportview-container .main .block-container {
         /* Adjust padding on the right to create empty space */
         padding-right: 16.66%;
