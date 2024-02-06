241a259f8752150589dc006b740c590dc4276f4b
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-06 10:27:57 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -53,6 +53,11 @@ st.markdown("""
     }
 
 
+st.markdown('<p class="big-font">JASON CHANG</p>', unsafe_allow_html=True)
+st.markdown('<div><p class="big2-font">PORTFOLIO</p><hr></div>', unsafe_allow_html=True)  # Adjusted for closer spacing and alignment
+st.markdown('<p class="med2-font">Full Stack Senior Data Analyst</p>', unsafe_allow_html=True)
+
+st.markdown("""
 
     /* Adjust padding and margins */
     .reportview-container .main .block-container { padding-top: 1rem; padding-right: 16.66%; }
