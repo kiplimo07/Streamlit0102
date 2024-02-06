81f4c2b1448c1888d403e5a6d43e2013859a2e82
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-05 16:19:00 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -22,10 +22,14 @@ st.markdown("""
 <style>
 .big-font {
     font-family: 'Raleway', sans-serif;
-    font-size: 90px !important;
+    font-size: 70px !important; /* Reduced size for shorter appearance */
     font-weight: 100;
     color: white;
+    display: inline-block; /* Makes the element respect the width */
+    max-width: 80%; /* Adjust as needed to control width */
+    margin: 0 auto; /* Center align if needed */
 }
+</style>
 .big2-font { font-family: 'Bebas Neue', sans-serif; font-size:70px !important; font-weight: 100; color: #D09E55; display: inline-block; margin-bottom: 0px; }
 .med2-font { font-family: 'Bebas Neue', sans-serif; font-size:45px !important; font-weight: 100; color: white; }
 .medium-font { font-family: 'Bebas Neue', sans-serif; font-size:55px !important; font-weight: 100; color: #D09E55; }
