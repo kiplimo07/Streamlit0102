d12d1e22ac1ac5b9c9bd54a86eef33abf8faeaf0
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-07 10:52:01 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -40,18 +40,10 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-def sidebar_navigation():
-    page_options = ["WELCOME", "DATA ANALYTICS / ENGAGEMENT & MONETIZATION", "DASHBOARD / EXECUTIVE BUSINESS INSIGHTS", "DATA ANALYSIS / WAREHOUSE & GL ACCOUNT OPTIMIZATION", "PROCESS AUTOMATION / QUARTERLY ROYALTY MANAGEMENT", "SCOPE OF SKILLS", "CERTIFICATIONS", "LET'S CONNECT"]
-    if 'selected_page' not in st.session_state:
-        st.session_state.selected_page = page_options[0]
+with st.sidebar:
+    st.markdown('<p class="medium-font">Navigation</p>', unsafe_allow_html=True)
+    page = st.radio("", ["WELCOME", "DATA ANALYTICS / ENGAGEMENT & MONETIZATION", "DASHBOARD / EXECUTIVE BUSINESS INSIGHTS", "DATA ANALYSIS / WAREHOUSE & GL ACCOUNT OPTIMIZATION", "PROCESS AUTOMATION / QUARTERLY ROYALTY MANAGEMENT", "SCOPE OF SKILLS", "CERTIFICATIONS", "LET'S CONNECT"])
 
-    selected_page = st.sidebar.radio("Navigation", page_options, on_change=st.experimental_rerun, key='page_radio')
-    
-    # Update the session state
-    st.session_state.selected_page = selected_page
-    return selected_page
-
-selected_page = sidebar_navigation()
 
 @st.cache
 def load_data(url):
@@ -318,4 +310,3 @@ elif page == "LET'S CONNECT":
     <br>
     <img src="https://img.icons8.com/material-outlined/24/000000/map-pin.png" alt="location_icon" style="vertical-align:middle; width:30px; height:30px;"> **Location:** Irvine, CA
     """, unsafe_allow_html=True)
-
