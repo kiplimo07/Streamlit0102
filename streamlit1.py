1a30d925a490fc7d844edea2744d4852b1204838
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-06 17:49:39 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -297,7 +297,7 @@ elif page == "Certifications":
 
 elif page == "Contact":
     st.header("LET'S CONNECT!!")
-    st.write("""
+    st.caption("""
     Feel free to connect with me for any inquiries or opportunities.
     
     - **Phone:** (626) 203 – 3319
