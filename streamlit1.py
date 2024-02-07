eea4b741e66fc1cd0697de0476ea6abde8cdb631
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-06 16:40:13 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -23,11 +23,11 @@ st.markdown("""
     .big2-font { font-family: 'Bebas Neue', sans-serif; font-size: 70px !important; font-weight: 100; color: #3b3f24; display: inline-block; margin-bottom: 0px; }
     .med2-font { font-family: 'Bebas Neue', sans-serif; font-size: 30px !important; font-weight: 100; color: #D09E55; }
     .medium-font { font-family: 'Bebas Neue', sans-serif; font-size: 45px !important; font-weight: 100; color: #D09E55; }
-    .small-font { font-family: 'Lato', sans-serif; font-size: 35px !important; color: #282D33; }
-    .markdown, .streamlit-container .markdown-text-container * { font-family: 'Lato', sans-serif !important; font-size: 30px !important; }
+    .small-font { font-family: 'Lato', sans-serif; font-size: 40px !important; color: #282D33; }
+    .markdown, .streamlit-container .markdown-text-container * { font-family: 'Lato', sans-serif !important; font-size: 40px !important; }
     .reportview-container .main { background-color: #1D262F; }
     .sidebar .sidebar-content { background-color: #1D262F; color: white; }
-    .fa { padding-right: 14px; }
+    .fa { padding-right: 5px; }
     hr { border-top: 2px solid #FFFFFF; width: 90%; margin-left: 0; margin-top: -20px; margin-bottom: 20px; }
 </style>
 """, unsafe_allow_html=True)
