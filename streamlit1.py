ef35c293bde0b3e82cd6ad5b57e72de4e3ab9b93
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-07 10:21:24 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -122,7 +122,7 @@ elif page == "DATA ANALYTICS / ENGAGEMENT & MONETIZATION":
         st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/777', caption='Distribution of Spending Across Skill Brackets', width=550)
 
     with col2:
-        st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/Picture/111', caption='Day-by-Day Churn Rate: Event 1 vs Event 2', width=550)
+        st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/111', caption='Day-by-Day Churn Rate: Event 1 vs Event 2', width=550)
 
     st.subheader("Methodology/Analytical Proficiency:")
     st.write("""
