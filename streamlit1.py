c7b8e8a32de922455e325822885c85782f19fa3e
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-06 17:53:18 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -297,11 +297,9 @@ elif page == "Certifications":
 
 elif page == "Contact":
     st.header("LET'S CONNECT!!")
-    st.subheader("""
-    Feel free to connect with me for any inquiries or opportunities.)
-     st.subheader("""
-    - **Phone:** (626) 203 – 3319)
-    - **Email:** [jason.chang01022021@gmail.com](mailto:jason.chang01022021@gmail.com)
-    - **LinkedIn:** [linkedin.com/in/jchang0102](https://linkedin.com/in/jchang0102)
-    - **Location:** Irvine CA
-    """)
+    st.subheader("Feel free to connect with me for any inquiries or opportunities.")
+    st.subheader("- **Phone:** (626) 203 – 3319")
+    st.subheader("- **Email:** [jason.chang01022021@gmail.com](mailto:jason.chang01022021@gmail.com)")
+    st.subheader("- **LinkedIn:** [linkedin.com/in/jchang0102](https://linkedin.com/in/jchang0102)")
+    st.subheader("- **Location:** Irvine CA")
+
