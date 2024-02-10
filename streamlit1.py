bd8bb2a9292307ae6f47379274e5a4dbceb3a9ae
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-09 21:04:14 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -22,7 +22,7 @@ st.markdown("""
 <style>
 .reportview-container .main .block-container { padding-right: 10% !important; }
 .big-font { font-family: 'Bebas Neue', sans-serif; font-size: 94px !important; font-weight: 100; color: #3e4047; display: inline-block; margin: 0 auto; margin-top: -40px; } /* Adjusted */
-.big2-font { font-family: 'Bebas Neue', sans-serif; font-size: 60px !important; font-weight: 100; color: #3e4047; display: inline-block; margin-bottom: 0px; margin-top: 10px; } /* Adjusted */
+.big2-font { font-family: 'Bebas Neue', sans-serif; font-size: 60px !important; font-weight: 100; color: #3e4047; display: inline-block; margin-bottom: 0px; margin-top: 20px; } /* Adjusted */
 .med2-font { font-family: 'Bebas Neue', sans-serif; font-size: 26px !important; font-weight: 100; color: #D09E55; margin-top: -20px; } /* Adjusted */
 .medium-font { font-family: 'Bebas Neue', sans-serif; font-size: 38px !important; font-weight: 100; color: #D09E55; }
 .small-font { font-family: 'Lato', sans-serif; font-size: 30px !important; color: #282D33; }
