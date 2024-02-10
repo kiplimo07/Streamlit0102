fc072a85ebb14382875a9afd671bfd61352fae1a
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-09 18:31:30 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -37,7 +37,7 @@ st.markdown('<p class="med2-font">Full Stack Senior Data Analyst</p>', unsafe_al
 st.markdown("""
 <style>
 html, body, .reportview-container, .main .block-container {
-    margin: 5;
+    margin: 0;
     padding: 5;
 }
 .reportview-container .main .block-container {
