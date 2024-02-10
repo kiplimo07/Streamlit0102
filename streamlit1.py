94bee27f37183f080b77997495283a65216c65d4
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-02-09 19:54:19 -0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -23,7 +23,7 @@ st.markdown("""
 .reportview-container .main .block-container { padding-right: 1% !important; }
 .big-font { font-family: 'Bebas Neue', sans-serif; font-size: 130px !important; font-weight: 100; color: #3e4047; display: inline-block; max-width: 80%; margin: 0 auto; margin-top: -65px; } /* Adjusted */
 .big2-font { font-family: 'Bebas Neue', sans-serif; font-size: 80px !important; font-weight: 100; color: #3e4047; display: inline-block; margin-bottom: 0px; margin-top: -10px; } /* Adjusted */
-.med2-font { font-family: 'Bebas Neue', sans-serif; font-size: 40px !important; font-weight: 100; color: #D09E55; margin-top: -5px; } /* Adjusted */
+.med2-font { font-family: 'Bebas Neue', sans-serif; font-size: 40px !important; font-weight: 100; color: #D09E55; margin-top: -10px; } /* Adjusted */
 .medium-font { font-family: 'Bebas Neue', sans-serif; font-size: 50px !important; font-weight: 100; color: #D09E55; }
 .small-font { font-family: 'Lato', sans-serif; font-size: 30px !important; color: #282D33; }
 .streamlit-container .markdown-text-container, .streamlit-container .markdown-text-container p, .streamlit-container .markdown-text-container li { font-family: 'Lato', sans-serif !important; font-size: 40px !important; color: #282D33; }
