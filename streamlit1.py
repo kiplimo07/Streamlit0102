7ec81080bd8340625c7ff129ab3c3e354cb3351a
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-03-15 22:45:08 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -189,7 +189,6 @@ elif page == "DATA ANALYTICS / ENGAGEMENT & MONETIZATION":
     st.pyplot(plt)
 
 
-st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/Picture/999', width=850)
 
 st.subheader("Result")
     st.write("""
