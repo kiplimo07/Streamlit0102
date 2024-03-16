5024b572e6c3fb4b9b89985bc7aa20dc23365184
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-03-15 22:18:39 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -197,22 +197,20 @@ Highlighted low spending in Platform 1, Region 5, indicating the need for furthe
 
 
  # Adding the final image at the correct indentation level
-    st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/Picture/999', width=850)
+ st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/Picture/999', width=850)
 
-    st.subheader("Result")
-    st.write("""
-        The data-driven approach yielded actionable insights, enabling the development of targeted strategies to enhance player retention and increase revenue. Specifically:
+st.subheader("Result")
+st.write("""
+    The data-driven approach yielded actionable insights, enabling the development of targeted strategies to enhance player retention and increase revenue. Specifically:
 
-A 21% increase in player engagement and spending in the identified high-value segments through targeted promotions and tailored in-game events.
-Initiated a strategic review and adjustment for underperforming segments, resulting in a marked improvement in player satisfaction and reduced churn rate by 15% during subsequent events.
-    """)
-    
-     st.subheader("Conclusion and Strategic Insights")
-    st.write("""
-       This analysis underscored the importance of leveraging advanced analytics to dissect player engagement and spending behaviors. By focusing on data-driven insights, we were able to develop targeted strategies that significantly enhanced player retention and increased revenue. Understanding the dynamics of player churn and spending across different segments and events provided a roadmap for more effective engagement strategies in future events, ensuring the continued success of Warcraft's in-game monetization efforts.
-    """)
+- A 21% increase in player engagement and spending in the identified high-value segments through targeted promotions and tailored in-game events.
+- Initiated a strategic review and adjustment for underperforming segments, resulting in a marked improvement in player satisfaction and reduced churn rate by 15% during subsequent events.
+""")
 
-
+st.subheader("Conclusion and Strategic Insights")
+st.write("""
+   This analysis underscored the importance of leveraging advanced analytics to dissect player engagement and spending behaviors. By focusing on data-driven insights, we were able to develop targeted strategies that significantly enhanced player retention and increased revenue. Understanding the dynamics of player churn and spending across different segments and events provided a roadmap for more effective engagement strategies in future events, ensuring the continued success of Warcraft's in-game monetization efforts.
+""")
 elif page == "DASHBOARD / EXECUTIVE BUSINESS INSIGHTS":
     st.header("DASHBOARD / EXECUTIVE BUSINESS INSIGHTS")
     
