7df0dd3ff907046abf4ec48d65068d89a8c31ef2
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-03-16 17:48:07 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -353,8 +353,7 @@ elif page == "CERTIFICATIONS":
  elif page == "LET'S CONNECT":
   
      st.header("LET'S CONNECT!!")
-        st.markdown("""
-     Feel free to connect with me for any inquiries or opportunities.
+     st.markdown("""Feel free to connect with me for any inquiries or opportunities.
      <br><br>
      <img src="https://img.icons8.com/material-rounded/48/000000/phone--v1.png" alt="phone_icon" style="vertical-align:middle; width:35px; height:35px;"> <span style="font-size:20px; vertical-align:middle;">**Phone:** (626) 203 – 3319</span>
      <br>
