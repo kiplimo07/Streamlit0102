d9cde0c4bf8afce9a405c4a59f440edf6af919ad
jasonchang0102 <154788234+jasonchang0102@users.noreply.github.com>
2024-03-16 17:41:44 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -350,15 +350,16 @@ elif page == "CERTIFICATIONS":
     st.subheader(" Analyzing and Visualizing Data with Power BI - EdX - 2019")
     st.image('https://github.com/jasonchang0102/Streamlit0102/raw/main/Picture/edx', width=800)
 
-    elif page == "LET'S CONNECT":
-    st.header(""LET'S CONNECT!!
+   elif page == "LET'S CONNECT":
+    st.header("""LET'S CONNECT!!
+
+
     
-              
-              IN GOD WE TRUST; FOR ALL ELSE, WE TURN TO THE VALIDATION OF DATA.  WITH DATA SCIENCE AS OUR COMPASS, WE'RE SET TO REVEAL HIDDEN INSIGHTS THAT OUR DATA IS JUST DYING TO TELL." 
+    IN GOD WE TRUST; FOR ALL ELSE, WE TURN TO THE VALIDATION OF DATA. WITH DATA SCIENCE AS OUR COMPASS, WE'RE SET TO REVEAL HIDDEN INSIGHTS THAT OUR DATA IS JUST DYING TO TELL. 
     J. CHANG
-    ")
- 
-    st.markdown("""
+    """)
+    
+        st.markdown("""
     Feel free to connect with me for any inquiries or opportunities.
     <br><br>
     <img src="https://img.icons8.com/material-rounded/48/000000/phone--v1.png" alt="phone_icon" style="vertical-align:middle; width:35px; height:35px;"> <span style="font-size:20px; vertical-align:middle;">**Phone:** (626) 203 – 3319</span>
@@ -369,3 +370,4 @@ elif page == "CERTIFICATIONS":
     <br>
     <img src="https://img.icons8.com/material-outlined/48/000000/map-pin.png" alt="location_icon" style="vertical-align:middle; width:35px; height:35px;"> <span style="font-size:20px; vertical-align:middle;">**Location:** Irvine, CA</span>
     """, unsafe_allow_html=True)
+
